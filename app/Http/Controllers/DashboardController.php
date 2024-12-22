fda8d9bafcc351fac21edebb56c3033a1e56fb06
shoaibasad <sunnymalik6772@gmail.com>
2024-12-22 14:44:39 +0500

    add the Artisan Command include

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -16,5 +17,28 @@ class DashboardController extends Controller
     }
             return view('pages.dashboard'); 
         
+    }
+    public function runMigrations()
+    {
+      
+        Artisan::call('migrate:fresh --seed');
+        
+        Artisan::call('cache:clear');
+        
+        Artisan::call('route:clear');
+        
+        Artisan::call('config:clear');
+        
+        Artisan::call('view:clear');
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'All commands executed successfully!',
+            'migrate_output' => Artisan::output(),
+            'cache_clear_output' => Artisan::output(),
+            'route_clear_output' => Artisan::output(),
+            'config_clear_output' => Artisan::output(),
+            'view_clear_output' => Artisan::output(),
+        ]);
     }
 }
